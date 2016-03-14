f7d28d63fd3743737e60bc52cf03afc3e32afd5a
eternaldainseif <lovemelancholy92@gmail.com>
2016-03-15 02:04:57 +0800

    Added Weather Model

--- CycoPath/Models/Weather.cs
+++ CycoPath/Models/Weather.cs
@@ -7,5 +7,13 @@ namespace CycoPath.Models
 {
     public class Weather
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Forecast { get; set; }
+        public string Icon { get; set; }
+        public string Zone { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Lon { get; set; }
     }
 }
