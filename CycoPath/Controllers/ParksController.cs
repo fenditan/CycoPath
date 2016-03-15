327668b449f1be974138fc3e96ee864bea9777ed
YamTai <YamTai.Ow_2014@SIT.SingaporeTech.edu.sg>
2016-03-16 03:02:16 +0800

    Routing (WIP)

--- CycoPath/Controllers/ParksController.cs
+++ CycoPath/Controllers/ParksController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using CycoPath.DAL;
 using CycoPath.Models;
 using System.Dynamic;
+using Jmelosegui.Mvc.GoogleMap;
 
 namespace CycoPath.Controllers
 {
@@ -31,10 +32,24 @@ namespace CycoPath.Controllers
             startEnd.Add(start);
             startEnd.Add(end);
 
-            ParksPathsModel.Parks = data.SearchPark(startEnd);
+            IEnumerable<Park> startEndPark = data.SearchPark(startEnd);
+            ParksPathsModel.Parks = startEndPark;
 
-            List <List<double[]>> paths = new List<List<double[]>>();
+            double[] startCoord = toDoubleArray(((List<Park>)startEndPark)[0].Coordinates.Split(','));
+            double[] endCoord = toDoubleArray(((List<Park>)startEndPark)[1].Coordinates.Split(','));
             
+            Dictionary<int,List<double[]>> paths = getPaths();
+            List<List<double[]>> route = getRoute(startCoord, endCoord, paths, 2);
+            ParksPathsModel.Paths = route;
+
+            return View(ParksPathsModel);
+
+        }
+
+        private Dictionary<int, List<double[]>> getPaths()
+        {
+            Dictionary<int, List<double[]>> result = new Dictionary<int, List<double[]>>();
+            int index = 0;
             string[] toParse;
 
             IEnumerable<Path> AllPaths = data.SelectAllPath();
@@ -43,20 +58,114 @@ namespace CycoPath.Controllers
             {
                 List<double[]> Coord = new List<double[]>();
                 string[] CoordPoints = path.Coordinates.Split(new string[] { "0 " }, StringSplitOptions.None);
-                foreach(string points in CoordPoints)
+                foreach (string points in CoordPoints)
                 {
                     toParse = points.Split(',');
                     double[] parseResult = { double.Parse(toParse[0]), double.Parse(toParse[1]) };
                     Coord.Add(parseResult);
                 }
-                paths.Add(Coord);
+                result.Add(index++, Coord);
             }
-
-            ParksPathsModel.Paths = paths;
-
-            return View(ParksPathsModel);
-
+            return result;
         }
 
+        private List<List<double[]>> getRoute(double[] start, double[] end, Dictionary<int, List<double[]>> allPaths, int A)
+        {
+            if (A == 0)
+            {
+                return new List<List<double[]>>();
+            }
+            double startToEnd = getDistance(start, end);
+            double currentMin = startToEnd;
+            int KeyToRemove = -1;
+
+            List<double[]> partialRoute = new List<double[]>();
+
+            foreach (KeyValuePair<int, List<double[]>> path in allPaths)
+            {
+                double[] pathStartCoord = path.Value.First();
+                double[] pathEndCoord = path.Value.Last();
+
+                double PathStartDistanceFromStart = getDistance(start, pathStartCoord);
+                double PathEndDistanceFromStart = getDistance(start, pathEndCoord);
+                double PathStartDistanceFromEnd = getDistance(end, pathStartCoord);
+                double PathEndDistanceFromEnd = getDistance(end, pathEndCoord);
+
+                //  if path goes further away from end, go to next iteration
+                //if ((PathStartDistanceFromEnd > startToEnd) || (PathEndDistanceFromEnd > startToEnd))
+                //{
+                //    continue;
+                //}
+
+                if ((currentMin > PathStartDistanceFromStart) || (currentMin > PathEndDistanceFromStart))
+                {
+                    if (PathStartDistanceFromStart < PathEndDistanceFromStart)
+                    {
+                        if (PathEndDistanceFromEnd < startToEnd)
+                        {
+                            currentMin = PathStartDistanceFromStart;
+                            start = pathEndCoord;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        if (PathStartDistanceFromEnd < startToEnd)
+                        {
+                            currentMin = PathEndDistanceFromStart;
+                            start = pathStartCoord;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    partialRoute = path.Value;
+                    KeyToRemove = path.Key;
+                }
+            }
+
+                //allPaths.Remove(KeyToRemove);
+                //if (KeyToRemove != 0)
+                //{
+                //    ;
+                //}
+                //else
+                //{
+                //    return new List<List<double[]>>();
+                //}           
+
+                List<List<double[]>> route = new List<List<double[]>>();
+
+            route.Add(partialRoute);
+            route.AddRange(getRoute(start, end, allPaths, --A));
+
+            return route;
+        }
+
+        private double getDistance(double[] pointA, double[] pointB)
+        {
+            double deltaLongSqr = Math.Pow((pointA[0] - pointB[0]), 2);
+            double deltaLatSqr = Math.Pow((pointA[1] - pointB[1]), 2);
+
+            return Math.Sqrt(deltaLongSqr + deltaLatSqr);
+        }
+
+        private double[] toDoubleArray(string[] toConvert)
+        {
+            int size = toConvert.Length;
+            double[] result = new double[size];
+
+            for (int i = 0; i<size; i++)
+            {
+                result[i] = double.Parse(toConvert[i]);
+            }
+
+            return result;
+        }
     }
 }
