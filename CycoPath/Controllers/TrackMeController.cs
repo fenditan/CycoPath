67d493c4eec43e1a8293c80ada64118b20ed2437
anitaKoo <anitakoo1994@gmail.com>
2016-04-01 15:17:17 +0800

    Add selected carpark session

--- CycoPath/Controllers/TrackMeController.cs
+++ CycoPath/Controllers/TrackMeController.cs
@@ -5,8 +5,14 @@ namespace CycoPath.Controllers
 {
     public class TrackMeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string hiddenTab3)
         {
+             if (String.IsNullOrEmpty(hiddenTab3) == false)
+            {
+                //hidden 3 = lat,long of selected carpark
+                HttpContext.Session["selectedCarpark"] = hiddenTab3;
+
+            }
             dynamic ParksPathsModel = new ExpandoObject();
             if (Session["ParksPathsModel"] != null)
             {
