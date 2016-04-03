c70c33c51133669aa72823470dca5d32327a6f5f
eternaldainseif <lovemelancholy92@gmail.com>
2016-04-03 15:48:52 +0800

    Park Markers Description

--- CycoPath/Models/CycoPathContext.Designer.cs
+++ CycoPath/Models/CycoPathContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Fendi\Documents\GitHub\CycoPath\CycoPath\Models\CycoPathContext.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\YEAR 2 TRIMESTER 2\ICT2106\Project\CycoPath\CycoPath\Models\CycoPathContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
